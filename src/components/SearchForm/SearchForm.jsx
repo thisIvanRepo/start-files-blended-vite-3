bdccdad89f21ca62f3de464f8145db37c5e996b4
AndriiPichkorskyi <desertptu4ka@gmail.com>
2025-05-17 19:17:50 +0300

    Done tasks 3-4

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,15 +1,55 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import styles from './SearchForm.module.css';
+import { useState } from 'react';
 
-// const regions = [
-//   { id: 'africa', value: 'africa', name: 'Africa' },
-//   { id: 'america', value: 'america', name: 'America' },
-//   { id: 'asia', value: 'asia', name: 'Asia' },
-//   { id: 'europe', value: 'europe', name: 'Europe' },
-//   { id: 'oceania', value: 'oceania', name: 'Oceania' },
-// ];
+const regions = [
+  { id: 'africa', value: 'africa', name: 'Africa' },
+  { id: 'america', value: 'america', name: 'America' },
+  { id: 'asia', value: 'asia', name: 'Asia' },
+  { id: 'europe', value: 'europe', name: 'Europe' },
+  { id: 'oceania', value: 'oceania', name: 'Oceania' },
+];
 
-const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+const SearchForm = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!value) return alert('Select any region');
+    onSubmit(value);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <button className={styles.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <select
+        aria-label="select"
+        className={styles.select}
+        name="region"
+        required
+        defaultValue="default"
+        onChange={handleChange}
+      >
+        <option disabled value="default">
+          Select a region
+        </option>
+        {regions.map(region => {
+          return (
+            <option key={region.id} value={region.value}>
+              {region.name}
+            </option>
+          );
+        })}
+      </select>
+    </form>
+  );
 };
 
 export default SearchForm;
--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,12 +1,50 @@
+import { useSearchParams } from 'react-router-dom';
 import Container from '../components/Container/Container';
 import Heading from '../components/Heading/Heading';
+import SearchForm from '../components/SearchForm/SearchForm';
 import Section from '../components/Section/Section';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from '../service/countryApi';
+import CountryList from '../components/CountryList/CountryList';
+import Loader from '../components/Loader/Loader';
 
 const SearchCountry = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const region = searchParams.get('region');
+    if (!region) return;
+
+    const getRegion = async () => {
+      setIsLoading(true);
+      try {
+        const data = await fetchByRegion(region);
+        setCountries(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getRegion();
+  }, [searchParams]);
+
+  const onSubmit = region => {
+    setSearchParams({
+      region,
+    });
+  };
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm onSubmit={onSubmit} />
+        {isLoading && <Loader />}
+        {countries.length > 0 && <CountryList countries={countries} />}
       </Container>
     </Section>
   );
